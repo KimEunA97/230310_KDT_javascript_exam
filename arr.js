206ef25e495e3c7a8bea3177263432b17d6ab6ee
KimEunA97 <kimeuna97@gmail.com>
2023-03-12 15:34:45 +0900

    설계 망해서 손 좀 대야함

--- arr.js
+++ arr.js
@@ -1,18 +1,17 @@
 const root = document.getElementById('root');
+const body = document.getElementsByTagName('body');
+body[0].style.margin = "0px"
+
 
 function tagMaker(tagname) {
 
-
   let element = document.createElement(tagname);
-
   return element;
-
 }
-
 
 const Allmember = _EXAMDATA.teamInformation.teamAllMember;
 
-//멤버별 정보 문자열 배열에 담기
+//멤버별 정보 배열
 memberName = [];
 memberGit = [];
 memberPosit = [];
@@ -25,20 +24,23 @@ for (key in Allmember) {
 
 }
 
-
-
 //이름
 const rootChild = root.children[0];
 
 infoArray = [memberName, memberGit, memberPosit];
 
+// title 생성
+const title = tagMaker("div");
+root.append(title)
 
+
+// 타이틀 다음 생성
 for (let i = 0; i < Allmember.length; i++) {
   //멤버 수만큼 박스 만들고
   let memberBox = tagMaker("span");
   root.append(memberBox);
 
-  // 멤버 수 안에 정보 박스 3개
+  // 멤버박스 안에 정보 박스 3개씩
   for (let i = 0; i < infoArray.length; i++) {
 
     let memberInfoBox = tagMaker("div");
@@ -49,37 +51,83 @@ for (let i = 0; i < Allmember.length; i++) {
 }
 
 
-console.log(infoArray)
 
+title.style.backgroundColor = "black"
+title.style.display = "flex"
+title.style.flexDirection = "column"
 
-root.children[0].children[0].textContent = memberName[0]
-root.children[1].children[0].textContent = memberName[1]
-root.children[2].children[0].textContent = memberName[2]
-root.children[3].children[0].textContent = memberName[3]
-root.children[4].children[0].textContent = memberName[4]
+title.style.justifyContent = "center";
+title.style.alignContent = "center";
+title.style.textAlign = "center";
+title.style.fontSize = "100px"
+title.style.fontWeight = "bold"
 
+title.style.width = "100vw"
+title.style.height = "100vh"
+title.style.color = "white"
 
-root.children[0].children[1].textContent = memberPosit[0]
-root.children[1].children[1].textContent = memberPosit[1]
-root.children[2].children[1].textContent = memberPosit[2]
-root.children[3].children[1].textContent = memberPosit[3]
-root.children[4].children[1].textContent = memberPosit[4]
-
-
-root.children[0].children[2].textContent = memberGit[0]
-root.children[1].children[2].textContent = memberGit[1]
-root.children[2].children[2].textContent = memberGit[2]
-root.children[3].children[2].textContent = memberGit[3]
-root.children[4].children[2].textContent = memberGit[4]
+title.textContent = _EXAMDATA.teamInformation.teamName;
 
 
 
 
+// children 1번부터 멤버 박스 
+for (let i = 1; i < 5; i++) {
+
+  root.children[i].style.display = "flex"
+  root.children[i].style.flexDirection = "column";
+  root.children[i].style.justifyContent = "center";
+  root.children[i].style.alignContent = "center";
+
+  root.children[i].style.width = "100%"
+  root.children[i].style.height = "100%"
+
+  root.children[i].style.fontSize = "80px"
+  root.children[i].style.fontWeight = "bold"
+  root.children[i].style.color = "white"
+
+  root.children[i].children[0].textContent = memberName[i]
+  root.children[i].children[1].textContent = memberPosit[i]
+  root.children[i].children[2].textContent = memberGit[i]
+
+
+}
+
+root.style.display = "flex";
+root.style.flexDirection = "column";
+root.style.width = "100vw"
+root.style.height = "100vh"
+root.style.textAlign = "center";
+root.style.alignItems = "center";
+root.style.justifyContent = "center";
 
 
 
+console.dir(root.style)
 
 
+
+root.children[1].style.display = "none"
+root.children[2].style.display = "none"
+root.children[3].style.display = "none"
+root.children[4].style.display = "none"
+root.children[5].style.display = "none"
+
+
+
+root.children[1].style.backgroundColor = "cyan"
+root.children[2].style.backgroundColor = "blue"
+root.children[3].style.backgroundColor = "yellow"
+root.children[4].style.backgroundColor = "red"
+root.children[5].style.backgroundColor = "green"
+
+
+root.children[i].addEventListener('click', function () {
+
+  root.children[i].setAttribute('display','none');
+  i++
+
+})
 
 
 // console.log(memberName)
